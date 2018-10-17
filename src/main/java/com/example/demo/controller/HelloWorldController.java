65d1c37bd922f5b9e6e76eb7dcaf0e33674047eb
fengcheZt <fengchecn@sina.com>
2018-10-17 11:02:14 +0800

    Update HelloWorldController.java

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-	@RequestMapping("/hello")
+	@RequestMapping("/hello1")
     public String index() {
         return "Hello World";
     }
