4c330362bce413f4fa2ab80145df8b5ef6e4c93b
fengcheZt <fengchecn@sina.com>
2018-10-17 16:23:04 +0800

    增加配置，增加测试

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -1,12 +1,17 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.NeoProperties;
+
 @RestController
 public class HelloWorldController {
-	@RequestMapping("/hello11")
+	@Autowired
+	NeoProperties neo;
+	@RequestMapping("/hello")
     public String index() {
-        return "Hello World";
+        return neo.getHellworld();
     }
 }
