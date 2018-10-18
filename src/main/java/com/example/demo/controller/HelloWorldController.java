6364755d2e95ab36c33381c24171b2a2bd5f7fd3
fengcheZt <fengchecn@sina.com>
2018-10-18 16:29:28 +0800

    gengxin

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -1,17 +1,34 @@
 package com.example.demo.controller;
 
+import java.util.UUID;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.NeoProperties;
 
-@RestController
+@Controller
+
 public class HelloWorldController {
 	@Autowired
 	NeoProperties neo;
-	@RequestMapping("/hello")
-    public String index() {
-        return neo.getHellworld();
+	@RequestMapping(value = "/hello")
+    public String hello(Model model) {
+		String name = neo.getHellworld();
+        model.addAttribute("name", name);
+        return "/hello";
+    }
+	@RequestMapping("/uid")
+    String uid(HttpSession session) {
+        UUID uid = (UUID) session.getAttribute("uid");
+        if (uid == null) {
+            uid = UUID.randomUUID();
+        }
+        session.setAttribute("uid", uid);
+        return session.getId();
     }
 }
